f9a4dc4253e9d1ccf41fbbc3174e9f7363b28b21
erictau <erictau04@gmail.com>
2022-07-12 16:38:57 -0700

    Updates Restaurant model and adds/updates templates

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     food_category = models.CharField(max_length=100)
-    yelp_url = models.TextField(max_length=255)
-    personal_rating = models.IntegerField()
+    yelp_url = models.CharField(max_length=100)
+    personal_rating = models.IntegerField(
+        validators = [MaxValueValidator(5), MinValueValidator(1)]
+    )
 
     def __str__(self):
         return self.name
