fe6c3e7f3e07d02cec28d7223fef4d6cfb3070ce
erictau <erictau04@gmail.com>
2022-07-12 17:10:12 -0700

    Adds index view and detail view functionality including url routes, view functions, and templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('restaurants/', views.restaurants_index, name='index'),
+    path('restaurants/<int:restaurant_id>', views.restaurants_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,18 @@ from .models import Restaurant
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def restaurants_index(request):
+    restaurants = Restaurant.objects.all()
+    return render(request, 'restaurants/index.html', {
+        'restaurants': restaurants
+    })
+
+def restaurants_detail(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    return render(request, 'restaurants/detail.html', {
+        'restaurant': restaurant
+    })
